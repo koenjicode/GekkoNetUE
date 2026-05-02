bc4099f5183c40a6bdf51cd7799c89bc90d38bfb
Jordan Addams <43590463+koenjicode@users.noreply.github.com>
2026-05-02 02:51:24 +0100

    Subsystem Function Tooltips

--- Source/GekkoNetUE/Public/GekkoNetSubsystem.h
+++ Source/GekkoNetUE/Public/GekkoNetSubsystem.h
@@ -34,29 +34,39 @@ public:
 	// Set a new session configuration, this session config will be used next time.
 	UFUNCTION(BlueprintCallable)
 	void SetSessionConfig(FGekkoSessionConfig NewConfig);
+	// Sets the local delay for an existing local player in the game.
 	UFUNCTION(BlueprintCallable)
 	bool SetLocalDelay(int32 PlayerIndex, int32 Delay);
+	// Sets the same local delay for all local players present in the game.
 	UFUNCTION(BlueprintCallable)
 	bool SetLocalDelayForAllPlayers(int32 Delay);
+	// Set how many frames the game should process in advance.
 	UFUNCTION(BlueprintCallable)
 	bool SetRunahead(int32 Runahead);
 	
-	
+	// Checks whether a session is currently running.
 	UFUNCTION(BlueprintPure)
 	bool IsSessionActive() const { return Session != nullptr; }
+	// Get how many frames difference between you and other clients. Should be used within deterministic loop calculations.
 	UFUNCTION(BlueprintPure)
 	float GetFramesAhead() const;
 	
+	// Get network stats in association from a player.
 	UFUNCTION(BlueprintPure)
 	bool GetNetworkStats(int32 PlayerIndex, FGekkoNetworkStats& OutStats) const;
+	// Get a player's ping.
 	UFUNCTION(BlueprintPure)
 	float GetPlayerPing(int32 PlayerIndex) const;
+	// Checks if the specified player is of a specified type.
 	UFUNCTION(BlueprintPure)
 	bool IsPlayerType(EGekkoPlayerType Type, int32 PlayerIndex);
+	// Checks if the player is local.
 	UFUNCTION(BlueprintPure)
 	bool IsLocalPlayer(int32 PlayerIndex);
+	// Checks if the player is remote.
 	UFUNCTION(BlueprintPure)
 	bool IsRemotePlayer(int32 PlayerIndex);
+	// Checks if the player is a spectator.
 	UFUNCTION(BlueprintPure)
 	bool IsSpectator(int32 PlayerIndex);
     
